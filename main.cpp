c5af44515591d08f0383723bcc3e107692a96415
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-09-18 12:30:27 +0100

    org

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 using namespace std;
 
-struct REGISTERS {
+struct REGISTERS
+{
     uint16_t pc;
     uint8_t r_i;
     uint8_t r_a = 0;
@@ -14,26 +15,29 @@ struct REGISTERS {
     uint8_t r_l = 0;
 };
 
-int main() {
+int main()
+{
     // std::cout << "8080 Emulator Init" << std::endl;
-    boot();
+    // boot();
     return 0;
 }
 
-int inc_pc() {
-    // uint8_t pc = 0;
-    // return pc;
+int inc_pc()
+{
+    // uint8_t pc = 0; // return pc;
 }
 
-int load_program() {
+int load_program()
+{
     // Place program's instructions into memory contiguously.
-
 }
 
-int read_memory(uint16_t addr) {
+int read_memory(uint16_t addr)
+{
     // TODO
 }
 
-int boot() {
-    array<uint8_t, 65536> mem = { 0 };
+int boot()
+{
+    array<uint8_t, 65536> mem = {0};
 }
